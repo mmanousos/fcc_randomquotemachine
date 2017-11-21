c228ee09f2cc4f25bb1d4b82905a3d2f3df70cf6
M. Manousos <mmanousos@gmail.com>
2017-11-21 15:05:20 -0800

    additional attempt for global variable scope with object

--- script.js
+++ script.js
@@ -1,54 +1,48 @@
+
 $(document).ready(function() {
-    // create an object containing the quote text and author name
-    // pass that out into general scope so it can be accessed by all functions
-    
-    
-    
-    function Quote(quote,author) { //create new Quote object
-        this.quote = quote;  // placeholder for quote
-        this.authName = author; // placeholder for author
-    };
-    
-    function getQuote() {  // a function to access the API
-        var url = "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";  // the URL for the API
-        $.getJSON(url, function(val) { // call the url and get the values
-            var quoteOutput = val.quoteText;
-            var authOutput = val.quoteAuthor;
-            var newQuote = new Quote(quoteOutput, authOutput); // assign values from the API to the placeholders
-            return newQuote; // return those values out of the function so they can be used by other functions
-            console.log(newQuote);
-        });
-    };
-    
-    
-    function loadQuote() {  
-        $("#quoteText").html("" + newQuote.quote + "");
-        
-        if (newQuote.authName === "") {
-            newQuote.authName = "Anonymous";
-        $("#quoteAuthor").html(" -" + newQuote.authName + "");
+  
+     
+    function getQuote() {
+        var url = "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"; 
+         $.getJSON(url, function(val) {
+           var quote = val.quoteText; 
+           var authName = val.quoteAuthor; 
+            if (authName === "") {
+                authName = "Anonymous";
+            };
+             
+        var quoteObject = {
+          quoteVal: quote,   
+          authVal: authName     
         };
-    }
-    
-         
-getQuote();
-   
-
-
-    
-        $("#new-quote").on("click", function() {
-            loadQuote(); 
+             return quoteObject;
         });
-        
-        $("#twitter-button").on("click", function(){
+    }
+    getQuote();
+    
+    
+    function loadQuote() {    
+        $("#quoteText").html("" + quoteObject.quoteVal + ""); 
+        $("#quoteAuthor").html(" -" + quoteObject.authVal + "");
+    }
+    loadQuote();
+    
+    $("#new-quote").on("click", function() {
+            loadQuote(); 
+    });
+    
+    
+    $("#twitter-button").on("click", function(){
             if (newQuote[authName] === "") {
                 newQuote[authName] = "Anonymous";
             };
             
-            window.open("https://twitter.com/intent/tweet?text=via%20Random%20Quote%20Machine%20by%20@therealmprove%20-%20I%20wanted%20to%20share%20this%20quote: '" + quoteObject[quote] + "' -" + newQuote[authName]); 
-        });
-        
+            window.open("https://twitter.com/intent/tweet?text=via%20Random%20Quote%20Machine%20by%20@therealmprove%20-%20I%20wanted%20to%20share%20this%20quote: '" + quoteObject.quoteVal + "' -" + quoteObject.authVal); 
+    });
+    
 });
+    
+
              
 
     
