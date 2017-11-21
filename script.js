f75e6db941eeb6d6a97e7a3c48386078b531677b
M. Manousos <mmanousos@gmail.com>
2017-11-20 19:07:16 -0800

    loads quotes and authors. continues to load without clicking.

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
 $(document).ready(function() {
-  /*  pushQuote(); */
-    
-   /* function pushQuote() {
-        $.getJSON("https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en", function(val) {
-        var getQuote = val.quoteText;    
-        var newQuote = ""; 
-        var getAuth = val.quoteAuthor;  
-        var newAuth = "";
-            newQuote += ("<div class='newQuote'>" + getQuote + "</div>");
-            newAuth += ("<div class='newAuth'>" + getAuth + "</div>");
-            
-        $("#quoteText").html(newQuote);
-        $("#quoteAuthor").html(newAuth);    
-    });
-};
-   */             
-    
-    // load first quote on page ready
-    // function pushQuote() {
-       $("#new-quote").on("click", function() { 
+    function loadQuote() {   
         $.getJSON("https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en", function(val) {
         var newQuote = "";
           newQuote += "<div class='newQuote'>" + val.quoteText + "</div>";
@@ -30,15 +11,19 @@ $(document).ready(function() {
       
             $("#quoteText").html(newQuote); 
             $("#quoteAuthor").html(newAuth);
+            
+            $("#new-quote").on("click", function() {
+                loadQuote(); 
+            }); 
       
       });
-       });
+            
+    };
+        loadQuote();
 });
-   // };
-    // pushQuote();
-    // $("#new-quote").on("click", function() {
-       // pushQuote();
-//    });
+    
+    
 
-
+    
+  
 
