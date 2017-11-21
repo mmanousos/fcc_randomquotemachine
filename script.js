082d0b890edb80c2173b2620fcd92133b07fb5c3
M. Manousos <mmanousos@gmail.com>
2017-11-21 09:25:18 -0800

    corrects blank author credit

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 $(document).ready(function() {
+    
+    
     function loadQuote() {   
         $.getJSON("https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en", function(val) {
         var newQuote = "";
           newQuote += "<div class='newQuote'>" + val.quoteText + "</div>";
             
         var authorName = val.quoteAuthor; 
+            if (authorName === "") {
+                authorName = "unknown";
+            };
         var newAuth = "";
-            newAuth += "<div class='newAuth'> - " + val.quoteAuthor + "</div>"; 
-                 console.log(authorName); 
+            newAuth += "<div class='newAuth'> - " + authorName + "</div>"; 
       
             $("#quoteText").html(newQuote); 
             $("#quoteAuthor").html(newAuth);
             
-            $("#new-quote").on("click", function() {
-                loadQuote(); 
             }); 
+            
+        $("#new-quote").on("click", function() {
+                loadQuote(); 
+            
       
       });
             
     };
-        loadQuote();
+       loadQuote(); 
 });
     
     
